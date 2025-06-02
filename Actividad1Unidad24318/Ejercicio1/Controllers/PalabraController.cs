0db11f73cc4140f975f9e2d37b8140e993e9e96f
Luis Fortuna U <lfortuna@dgii.gov.do>
2025-06-02 17:37:44 -0400

    termine mi ejercicio 2

---
+++ Actividad1Unidad24318/Ejercicio1/Controllers/PalabraController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PalabraController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string oracion)
+        {
+            if (string.IsNullOrEmpty(oracion))
+            {
+                return BadRequest("La oración no puede estar vacía.");
+            }
+
+            var palabras = oracion.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            return Ok(palabras.Count());
+        }
+    }
+}
