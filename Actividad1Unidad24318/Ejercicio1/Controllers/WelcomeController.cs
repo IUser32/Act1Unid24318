45f4a97ea0e29dcc437f6691bea9593a75f3a3fb
Luis Fortuna U <lfortuna@dgii.gov.do>
2025-06-02 17:22:17 -0400

    agregando el primer ejercicio.

---
+++ Actividad1Unidad24318/Ejercicio1/Controllers/WelcomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WelcomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetWelcomeMessage(string name)
+        {
+            return Ok(string.Format("Hola {0}, un placer conocerte", name));
+        }
+    }
+}
